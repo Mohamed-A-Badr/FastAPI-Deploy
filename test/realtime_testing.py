f8182735d54d80b595c2b3638c73fe454b50cbae
Mohamed-A-Badr <badr49960@gmail.com>
2023-07-15 04:28:44 +0300

    Update Average sensor's data

--- test/realtime_testing.py
+++ test/realtime_testing.py
@@ -1,35 +1,33 @@
 import asyncio
 import httpx
+import random
 
 
 async def send_request():
+    temperature = round(random.uniform(35.0, 38.0), 1)
+    pulse_rate = round(random.uniform(80.0, 100.0), 1)
+    oximeter = round(random.uniform(120.0, 130.0), 1)
     async with httpx.AsyncClient() as client:
         headers = {
             "Content-Type": "application/json",
+            "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMzLCJpYXQiOjE2ODkzNTE5NTEsIm5iZiI6MTY4OTM1MTk1MSwianRpIjoiYWZmZDU4YTQtZWE0ZC00YjUxLTgyMWUtYzkzY2UyY2VjNGE4IiwiZXhwIjoxNzIwODg3OTUxLCJ0eXBlIjoiYWNjZXNzIiwiZnJlc2giOmZhbHNlfQ.YFEO5b_3z0PZuxMHm6t4dT4xfdXTRXZ68hV-jpZVQjA",
         }
         payload = {
-            "temperature": "36.5",
-            "oximeter": "125.4",
-            "pulse_rate": "90.5",
-            "sugar_level": "70.45",
-            "chair_id": "12315",
+            "temperature": temperature,
+            "oximeter": oximeter,
+            "pulse_rate": pulse_rate,
         }
-        response = await client.post("http://127.0.0.1:8000/chair/data", json=payload)
-        print(response.text)
-
-
-async def get_request():
-    async with httpx.AsyncClient() as client:
-        response = await client.get("http://127.0.0.1:8000/chair/data/12315")
+        # http://13.48.27.223:8000/
+        response = await client.post(
+            "http://127.0.0.1:8000/chair/data", headers=headers, json=payload
+        )
         print(response.text)
 
 
 async def main():
     while True:
         await send_request()
-        await asyncio.sleep(0.5)
-        await get_request()
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(5)
 
 
 if __name__ == "__main__":
